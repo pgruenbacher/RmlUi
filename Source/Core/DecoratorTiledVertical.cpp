37f12b7ca9ae61e22efb2315d168066412904986
paul gruenbacher <pgruenbacher@gmail.com>
2023-09-08 22:36:29 -0500

    range progress

--- Source/Core/DecoratorTiledVertical.cpp
+++ Source/Core/DecoratorTiledVertical.cpp
@@ -92,13 +92,11 @@ DecoratorDataHandle DecoratorTiledVertical::GenerateElementData(Element* element
 	Vector2f bottom_dimensions = tiles[BOTTOM].GetNaturalDimensions(element);
 	Vector2f centre_dimensions = tiles[CENTRE].GetNaturalDimensions(element);
 
-	Log::Message(Log::LT_INFO, "tbottom_dimensions %f %f %f", tiles[BOTTOM].display_scale, top_dimensions[0], top_dimensions[1]);
 	// Scale the tile sizes by the width scale.
 	ScaleTileDimensions(top_dimensions, padded_size.x, Axis::Horizontal);
 	ScaleTileDimensions(bottom_dimensions, padded_size.x, Axis::Horizontal);
 	ScaleTileDimensions(centre_dimensions, padded_size.x, Axis::Horizontal);
 
-	Log::Message(Log::LT_INFO, "tbottom_dimensions_scaled %f %f %f", tiles[BOTTOM].display_scale, top_dimensions[0], top_dimensions[1]);
 	// Round the outer tile heights now so that we don't get gaps when rounding again in GenerateGeometry.
 	top_dimensions.y = Math::Round(top_dimensions.y);
 	bottom_dimensions.y = Math::Round(bottom_dimensions.y);
